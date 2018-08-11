350ee1cf0a973e4f496b81644283d3833092669f
Kirsty Devlin <kirsty.leanne.devlin@gmail.com>
2018-08-11 18:54:43 +0100

    returns buzz when given the number 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,5 +12,9 @@ describe 'fizzbuzz' do
   it 'returns a number' do
     expect(fizzbuzz(4)).to eq(4)
   end
+
+  it 'returns "buzz" when given the number 5' do
+    expect(fizzbuzz(5)).to eq("buzz")
+  end
     
 end
